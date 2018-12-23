2cb10ef5dbb8a4bdb2db25dda639e720d2968f62
Chijioke Kamanu <jioke.k@gmail.com>
2018-12-23 09:35:45 -0500

    Create Movie.h

---
+++ StreamingService/Movie.h
@@ -0,0 +1,13 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+
+class Movie : public Video {
+  public:            //public class functions
+    Movie();
+    Movie(string, int, string, string, string);
+    void PrintItem();
+  private:          //private variables
+    bool has_sequel;
+};
+
+#endif           //Indicate end of header file
